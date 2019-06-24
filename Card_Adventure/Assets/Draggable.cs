78ce3b2837fe85a4f7d8077386fc896b93217bb6
Kersjes, Alex G <alexkersjes@live.nl>
2019-06-24 21:34:12 +0200

    Card dragging

---
+++ Card_Adventure/Assets/Draggable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IDropHandler
+{ 
+
+    Vector3 offset;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        offset = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        offset = offset - (Vector3)eventData.position;
+        this.transform.position -= offset;
+        offset = eventData.position;
+    }
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+
+    }
+
+
+}
