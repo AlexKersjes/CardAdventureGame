bd6de810f50c511a7cee7eb6c4f93cf89645a063
Kersjes, Alex G <alexkersjes@live.nl>
2019-06-25 00:43:39 +0200

    more card dragging

--- Card_Adventure/Assets/Draggable.cs
+++ Card_Adventure/Assets/Draggable.cs
@@ -1,35 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IDropHandler
+public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 { 
 
     Vector3 offset;
+    Transform oldParent;
+    GameObject placeholder;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         offset = eventData.position;
+        oldParent = this.transform.parent;
+        int i = this.transform.GetSiblingIndex();
+        this.transform.SetParent(this.transform.parent.parent);
+
+
+        placeholder = new GameObject();
+        placeholder.transform.SetParent(oldParent);
+        placeholder.transform.SetSiblingIndex(i);
+        LayoutElement layout = placeholder.AddComponent<LayoutElement>();
+        layout.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        layout.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        offset = offset - (Vector3)eventData.position;
-        this.transform.position -= offset;
+        offset =  (Vector3)eventData.position - offset ;
+        this.transform.position += offset;
         offset = eventData.position;
     }
 
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        int i = placeholder.transform.GetSiblingIndex();
+        Destroy(placeholder);
 
+        transform.SetParent(oldParent);
+        transform.SetSiblingIndex(i);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 
-    public void OnDrop(PointerEventData eventData)
-    {
-
-    }
 
 
 }
